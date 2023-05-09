b10cf052e4ec058c65ad87dcd3f71836fc34723e
MarekSarvas <sarvas.sarvas@gmail.com>
2023-05-09 16:09:15 +0300

    fixed script for finetuning/inference of pre-trained AV-Hubert

--- run_finetuning.sh
+++ run_finetuning.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-#SBATCH --time=16:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
 #SBATCH --output=finetune_av_base_iter5.out
@@ -9,28 +8,31 @@
 
 
 module load miniconda
-
 source activate avhubert
 
 HUBERT_PATH="/scratch/work/sarvasm1/av_hubert/avhubert"
 EXP_PATH="/scratch/work/sarvasm1/AV-HuBERT-ClusterID-ASR/exp/finetuning"
-
 cd ${HUBERT_PATH}
 
 export HYDRA_FULL_ERROR=1
 
-conf_path="/scratch/work/sarvasm1/av_hubert/avhubert/conf/av-finetune"
-conf_name="base_lrs3_30h.yaml"
+# data
 datadir="/m/teamwork/t40511_asr/c/LRS3-TED/lrs3/30h_data"
 labeldir="/m/teamwork/t40511_asr/c/LRS3-TED/lrs3/30h_data"
-tokenizer="/m/teamwork/t40511_asr/c/LRS3-TED/lrs3/spm5000/spm_unigram5000.model"
-model_path="/scratch/work/sarvasm1/av_hubert/models/base_lrs3_iter5.pt"
 
-#model_path="/scratch/work/sarvasm1/av_hubert/models/finetuned/base_lrs3_30h.pt"
+# models
+tokenizer="/m/teamwork/t40511_asr/c/LRS3-TED/lrs3/spm5000/spm_unigram5000.model"
+finetuning_model_path="/scratch/work/sarvasm1/av_hubert/models/base_lrs3_iter5.pt"
+
+# config and experiments
+conf_path="/scratch/work/sarvasm1/av_hubert/avhubert/conf/av-finetune"
+conf_name="base_lrs3_30h.yaml"
 exp="finetune_AV_lrs3_base_iter5_5000vocab"
+
 start_stage=0
 stop_stage=1
 
+# finetune model on 30h lrs3 data
 if [ ${start_stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
 
 	cp ${conf_path}/${conf_name} ${EXP_PATH}/${exp}/config.yaml
@@ -40,33 +42,30 @@ if [ ${start_stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
 				task.data=${datadir} \
 				task.label_dir=${labeldir} \
 				task.tokenizer_bpe_model=${tokenizer} \
-				model.w2v_path=${model_path} \
+				model.w2v_path=${finetuning_model_path} \
 				hydra.run.dir=${EXP_PATH}/${exp} \
 				common.user_dir=`pwd`
 fi
 
+# run decoding on model from ${exp} directory
 if [ ${start_stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-	decode_conf_name="s2s_decode.yaml"
 
-	decode_dir=${EXP_PATH}/${exp}/decode_audio/s2s
+	# choose model for decoding - depending on experiment directory
+	decode_model_path="${EXP_PATH}/${exp}/checkpoints/checkpoint_best.pt"
+
+	# configuration and result dir
+	decode_conf_name="s2s_decode.yaml"
+	decode_dir=${EXP_PATH}/${exp}/decode_av/s2s
 	
 	mkdir -pv  ${decode_dir}
-
-	#cp ${conf_path}/${decode_conf_name} ${decode_dir}/config.yaml
 
 	python -B infer_s2s.py --config-dir "/scratch/work/sarvasm1/av_hubert/avhubert/conf/"  \
 						--config-name ${decode_conf_name} \
 						dataset.gen_subset=test \
-						common_eval.path=${model_path} \
+						common_eval.path=${decode_model_path} \
 						common_eval.results_path=${decode_dir} \
 						override.modalities=['audio','video'] \
-						+override.data="/m/teamwork/t40511_asr/c/LRS3-TED/lrs3/30h_data" +override.label_dir="/m/teamwork/t40511_asr/c/LRS3-TED/lrs3/30h_data" \
+						+override.data="/m/teamwork/t40511_asr/c/LRS3-TED/lrs3/30h_data" \
+						+override.label_dir="/m/teamwork/t40511_asr/c/LRS3-TED/lrs3/30h_data" \
 						common.user_dir=`pwd` > ${decode_dir}/ref_hyp.out
 fi
-
-
-
-
-
-
-
